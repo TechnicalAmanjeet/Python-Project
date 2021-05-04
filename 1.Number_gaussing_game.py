de5b218740c35c289a993a30e7ee7953028aecb3
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-05-04 15:21:13 +0530

    in this version of code i have added some new functionality like give user 3 choice and uses function as well..

--- 1.Number_gaussing_game.py
+++ 1.Number_gaussing_game.py
@@ -1,25 +1,54 @@
 import random 
+import os
 
-print("""********** Number Gausing Game Project *********""")
+def gauss(min,max):
+  result=random.randint(min,max)
+  while(1):
+    number=int(input(f"Enter the gaussing number beetween {min} & {max} :"))
+    if(number==result):
+      print("\nYou gaused right..","gaussed no. is : ",number)
+      input("\n please press enter to go ahead...")
+      break
+    elif(number>result):
+      print("\nYou are to high.. ")
+      max=number
+    else:
+      print("\nyou are two low...")
+      min=number
+
+
 while(1):
-  min,max=map(int,input("Enter min and max no. in which you want to gauss : ").split())
-  if(max<=min):
-    print("you Entered value in wrong order.. please enter again in correct order...")
-  else:
-    break
+  os.system("clear")
+  print("""********** Number Gausing Game Project *********\n\n""")
 
-result=random.randint(min,max)
+  print("1. Do you want to gauss no. beetween by default no... ")
+  print("2. or Do you want to gauss no. beetween your choice...")
+  print("3. Type 3 to exit...")
+  ch= input("\nEnter your choice : ")
 
-while(1):
-  number=int(input("Enter the gaussing number : "))
-  if(number==result):
-    print("You gaused right..","gaussed no. is : ",number)
+  if(int(ch)==1):
+    gauss(1,100)
+  elif(int(ch)==2):
+    while(1):
+      min,max=map(int,input("Enter min and max no. in which you want to gauss : ").split())
+      if(max<=min):
+        print("\nyou Entered value in wrong order.. please enter again in correct order...")
+      else:
+        gauss(min,max)
+        break
+      
+  elif(int(ch)==3):
     break
-  elif(number>result):
-    print("You are to high.. ")
   else:
-    print("you are two low...")
-  
+    print("\nyou have entered wrong choice.. please read carefully and then enter : ")
+    input("\n Type enter to go ahead...")
+
+
+
+
+
+
+
 
 
 
